9746e7153112e726b33d30912e7134f3ca332ff0
vagrant <vagrant@homestead>
2018-04-15 11:57:13 +0000

    Topic order

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -16,7 +16,7 @@
         <div class="panel panel-default">
 
             <div class="panel-heading">
-                <ul class="nav nav-pills">
+             <ul class="nav nav-pills">
                     <li class="{{ active_class( ! if_query('order', 'recent') ) }}"><a href="{{ Request::url() }}?order=default">最后回复</a></li>
                     <li class="{{ active_class(if_query('order', 'recent')) }}"><a href="{{ Request::url() }}?order=recent">最新发布</a></li>
                 </ul>
