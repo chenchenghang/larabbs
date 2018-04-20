0bd749b6e64a21c0ca9408632e90a9c80d93e253
vagrant <vagrant@homestead>
2018-04-20 09:09:28 +0000

    Integrated administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -55,13 +55,23 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+
+                            @can('manage_contents')
+                                <li>
+                                    <a href="{{ url(config('administrator.uri')) }}">
+                                        <span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span>
+                                        管理后台
+                                    </a>
+                                </li>
+                            @endcan
+
                             <li>
                                 <a href="{{ route('users.show', Auth::id()) }}">
                                     <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                                     个人中心
                                 </a>
                             </li>
-                            <li>
+                             <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
                                     <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                                     编辑资料
